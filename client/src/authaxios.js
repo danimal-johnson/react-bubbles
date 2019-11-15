04a9471b9c946eb3a46075bf59b568f8ada6f7a6
Dan Johnson <danimal.johnson@gmail.com>
2019-11-15 09:54:14 -0800

    AxiosWithAuth, private route, color fetch completed

---
+++ client/src/authaxios.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import axios from 'axios';
+
+const axiosWithAuth =() => {
+  const token = localStorage.getItem('token');
+
+  return axios.create({
+      headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `${token}`,
+      },
+  });
+};
+
+const authAxios = axiosWithAuth();
+
+export default authAxios;
--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
+// import axios from "axios";
+import authAxios from "../authaxios";
 
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
@@ -8,6 +9,17 @@ const BubblePage = () => {
   const [colorList, setColorList] = useState([]);
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
+  useEffect(() => {
+    authAxios
+    .get("http://localhost:5000/api/colors")
+    .then(response => {
+      console.log(response);
+      setColorList(response.data);
+    })
+    .catch(error => {
+      console.log(error);
+    })
+  },[])
 
   return (
     <>
